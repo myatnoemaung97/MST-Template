f1bcbd379d875588a85f06a7d4cccd75282fd2a5
myatnoemaung97 <myatnoemaung001@gmail.com>
2023-12-01 10:23:47 +0630

    header fix

--- script.js
+++ script.js
@@ -37,3 +37,24 @@ function clickQuestion(event) {
     question.style.color = '#000000';
   }
 }
+
+window.addEventListener('scroll', function() {
+  var element = document.querySelector('header');
+  var scrollPosition = window.scrollY;
+
+  const addressBar  = this.document.getElementById('address-bar');
+  const navBar = this.document.getElementById('nav-bar');
+
+  if (scrollPosition > 500) {
+    
+    addressBar.classList.add('d-none');
+    addressBar.classList.remove('d-md-flex');
+   
+    navBar.style.backgroundColor = '#010035';
+  } else {
+    addressBar.classList.add('d-md-flex');
+    addressBar.classList.remove('d-none');
+   
+    navBar.style.backgroundColor = '';
+  }
+});
